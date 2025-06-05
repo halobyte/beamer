a88158d24acd05a4bd3a6199e8d738e756854a96
devsheke <abhi@sheke.tech>
2025-06-05 22:32:00 +0530

    update env list

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -5,10 +5,17 @@ export const env = createEnv({
   shared: {
     NODE_ENV: z.enum(["production", "development"]).default("development"),
   },
+  client: {
+    NEXT_PUBLIC_DASHBOARD_URL: z
+      .string()
+      .url()
+      .default("http://localhost:3001"),
+  },
   server: {
     DATABASE_URL: z.string().url(),
   },
   runtimeEnv: {
+    NEXT_PUBLIC_DASHBOARD_URL: process.env.DASHBOARD_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
