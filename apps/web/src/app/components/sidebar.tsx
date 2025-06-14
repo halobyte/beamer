7278367961fa4cbeb0d34d5d24a8290e963f0bff
devsheke <abhi@sheke.tech>
2025-06-14 11:35:22 +0530

    modify sidebar height and positioning

--- apps/web/src/app/components/sidebar.tsx
+++ apps/web/src/app/components/sidebar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { Bug, HelpCircle } from "lucide-react";
 
-import BeamerIcon from "@beamer/ui/components/beamer-icon";
 import {
   Sidebar as _Sidebar,
   SidebarContent,
@@ -9,7 +8,6 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
-  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
@@ -19,22 +17,11 @@ import { links } from "./nav-bar";
 
 export default function Sidebar() {
   return (
-    <_Sidebar className="!sm:hidden border-none">
+    <_Sidebar className="mt-[61px] h-[calc(100vh-61px)]">
       <SidebarContent>
-        <SidebarHeader className="mt-[9px] flex h-[60px] flex-row items-center gap-3 border border-transparent px-3.5 *:cursor-default">
-          <span className="relative inline-flex size-10 items-center justify-center rounded-md bg-gradient-to-b from-sky-700 via-sky-700 to-sky-800 text-yellow-300">
-            <BeamerIcon className="absolute size-6 fill-white" />
-          </span>
-          <div className="flex flex-col">
-            <h1 className="text-lg font-semibold tracking-tight">Beamer</h1>
-            <a className="text-sm font-medium text-sidebar-foreground/70">
-              Easy email campaigns
-            </a>
-          </div>
-        </SidebarHeader>
         <SidebarGroup>
           <SidebarGroupLabel>Navigation</SidebarGroupLabel>
-          <SidebarGroupContent>
+          <SidebarGroupContent className="top-10">
             <SidebarMenu>
               {links.map((link) => (
                 <SidebarMenuItem key={link.title}>
